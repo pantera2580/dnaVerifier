862ef0a4ecf8ab7f82d7a451144e7def96b31053
pantera2580 <emmnlcruz@gmail.com>
2022-05-28 08:08:49 -0300

    initial commit

---
+++ src/main/java/com/mec/dnaVerifier/DnaVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.mec.dnaVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnaVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnaVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mec/dnaVerifier/DnaVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mec.dnaVerifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnaVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
